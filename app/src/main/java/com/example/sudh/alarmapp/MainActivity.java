4b692120b6e4657685ba17124bc900cbe3b9adde
sudo-chawhan <cs16btech11037@iith.ac.in>
2019-02-15 18:13:20 +0530

    add RTC alarm timer

--- app/src/main/java/com/example/sudh/alarmapp/MainActivity.java
+++ app/src/main/java/com/example/sudh/alarmapp/MainActivity.java
@@ -23,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private void setAlarm(Context context){
         // set a repeating alarm
         int seconds = 1;
-        int alarmHr = 19;
-        int alarmMn = 48;
+        int alarmHr = 17;
+        int alarmMn = 51;
         Intent alarmIntent = new Intent(context, AlarmReceiver.class);
         PendingIntent sender = PendingIntent.getBroadcast(context, 0, alarmIntent, PendingIntent.FLAG_CANCEL_CURRENT);
 
@@ -32,22 +32,21 @@ public class MainActivity extends AppCompatActivity {
         // get a Calendar object with current time
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
-//        calendar.set(Calendar.HOUR_OF_DAY, alarmHr);
-//        calendar.set(Calendar.MINUTE, alarmMn);
-        ((AlarmManager) getSystemService(ALARM_SERVICE)).set(
-                AlarmManager.RTC, System.currentTimeMillis() + seconds
-                        * 1000, sender);
+        calendar.set(Calendar.HOUR_OF_DAY, alarmHr);
+        calendar.set(Calendar.MINUTE, alarmMn);
+//        ((AlarmManager) getSystemService(ALARM_SERVICE)).set(
+//                AlarmManager.RTC, calendar.getTimeInMillis(), sender);
 //        Toast.makeText(MainActivity.this, "Timer set to " + seconds + " seconds.",
 //                Toast.LENGTH_SHORT).show();
-//        AlarmManager am = (AlarmManager) context.getSystemService(context.ALARM_SERVICE);
+        AlarmManager am = (AlarmManager) context.getSystemService(context.ALARM_SERVICE);
 //
-//        am.cancel(sender); // cancel others.
+        am.cancel(sender); // cancel others.
 //
 //        am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+1000,
 //                10*1000, sender);
 
-//        am.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis()+100,
-//                AlarmManager.INTERVAL_DAY, sender);
+        am.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                AlarmManager.INTERVAL_DAY, sender);
     }
 
 }
