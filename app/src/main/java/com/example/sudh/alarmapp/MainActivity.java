dd8399c79d72f487296975aa6c7cc54b5d9b7366
sudo-chawhan <cs16btech11037@iith.ac.in>
2019-02-15 18:52:13 +0530

    timely commit

--- app/src/main/java/com/example/sudh/alarmapp/MainActivity.java
+++ app/src/main/java/com/example/sudh/alarmapp/MainActivity.java
@@ -5,26 +5,47 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
+    SharedPreferences sharedPref;
+
+    String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setAlarm(getApplicationContext());
+        sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+//        SharedPreferences.Editor editor = sharedPref.edit();
+//        editor.putInt(getString(R.string.saved_high_score_key), newHighScore);
+//        editor.commit();
+        final TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "alarm set for " + timePicker.getHour() + ":"+ timePicker.getMinute(), Toast.LENGTH_LONG);
+                setAlarm(getApplicationContext(), timePicker.getHour(), timePicker.getMinute());
+            }
+        });
+
     }
-    private void setAlarm(Context context){
+    private void setAlarm(Context context, int alarmHr, int alarmMn){
         // set a repeating alarm
         int seconds = 1;
-        int alarmHr = 17;
-        int alarmMn = 51;
+//        int alarmHr = 17;
+//        int alarmMn = 51;
+//        Log.d(TAG, "setAlarm: entered");
         Intent alarmIntent = new Intent(context, AlarmReceiver.class);
         PendingIntent sender = PendingIntent.getBroadcast(context, 0, alarmIntent, PendingIntent.FLAG_CANCEL_CURRENT);
 
